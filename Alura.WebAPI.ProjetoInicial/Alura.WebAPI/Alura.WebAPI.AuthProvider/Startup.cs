21fbf074bdd911cc0d2f2a17ce008330b25a90cd
Gabriela Vilela <gabrielathalitasv@gmail.com>
2020-02-18 11:50:13 -0300

    Criando um projeto separad para a autenticação

---
+++ Alura.WebAPI.ProjetoInicial/Alura.WebAPI/Alura.WebAPI.AuthProvider/Startup.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.Seguranca;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alura.WebAPI.AuthProvider
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AuthDbContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("AuthDB"));
+            });
+
+            services.AddIdentity<Usuario, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            }).AddEntityFrameworkStores<AuthDbContext>();
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
